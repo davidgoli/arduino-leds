1e2c86bba80e12a7131ec173c90e9b98f0d99c7c
David Golightly <david.golightly@leapyear.io>
2022-10-21 15:16:50 -0700

    tame the twinkles

--- twinkles/twinkles.ino
+++ twinkles/twinkles.ino
@@ -8,14 +8,14 @@
 CRGB leds[NUM_LEDS];
 
 struct Twinkle {
-  int duration;
-  int intensity;
-  int darkInterval;
+  long duration;
+  float intensity;
+  long darkInterval;
   long startTime;
   int ledNum;
 };
 
-#define NUM_TWINKLES 20
+#define NUM_TWINKLES 10
 
 struct Twinkle twinkles[NUM_TWINKLES];
 
@@ -44,19 +44,31 @@ void loop() {
     leds[i] = CRGB::Black;
   }
 
-  twinkly(max(0, sin(millis() / 60000)+1));
-  bisexual(max(0, sin(10000 + (millis() / 60000))));
-  seagreen(max(0, sin(20000 + (millis() / 60000))));
+  twinkly(1);
+  bisexual(sin(10000 + (millis() / 60000))+1);
 
   FastLED.show();
 //  delay(10);
 }
 
+bool ledOccupied(Twinkle twinkles[], int ledNum) {
+  for (int i=0; i<NUM_TWINKLES; i++) {
+    if (twinkles[i].ledNum == ledNum) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 void initializeTwinkle(Twinkle twinkles[], int i) {
   twinkles[i].duration = random(2000, 10000);
+  int ledNum = random(0, NUM_LEDS);
   do {
-    twinkles[i].ledNum = random(0, NUM_LEDS);
-  } while (leds[twinkles[i].ledNum]);
+    ledNum += 1;
+  } while (ledOccupied(twinkles, ledNum));
+  twinkles[i].ledNum = ledNum;
+
   twinkles[i].intensity = random(BRIGHTNESS / 2, BRIGHTNESS);
   twinkles[i].startTime = millis();
   twinkles[i].darkInterval = random(0, 1000);
@@ -65,40 +77,33 @@ void initializeTwinkle(Twinkle twinkles[], int i) {
 void twinkly(float factor) {
   for (int i=0; i<NUM_TWINKLES; i++) {
     Twinkle twinkle = twinkles[i];
-    float maxAge = twinkle.startTime + twinkle.duration + twinkle.darkInterval;
-    if (millis() >= maxAge) {
-      initializeTwinkle(twinkles, i);
-    }
     float age = millis() - twinkle.startTime;
+    float maxAge = twinkle.duration + twinkle.darkInterval;
+    if (age > maxAge+10) {
+      initializeTwinkle(twinkles, i);
+      continue;
+    }
 
     float maxPt = twinkle.duration * 0.5;
     float b = max(0, (1.0 - (abs(age - maxPt) / maxPt)) * (float)twinkle.intensity);
 
     leds[twinkle.ledNum] += CHSV(0, 0, b * factor);
+    if (i == 10) {
+      Serial.println(b);
+    }
   }
 }
 
 void bisexual(float factor) {
   for (int i=0; i<NUM_LEDS; i++) {
-    double b = 32 * (1 + sin((i * 0.2) + (millis() * 0.00042)));
-    double g = 32 * (1 + sin((i * 0.2) + (millis() * 0.00036)));
-  //      Serial.println(g);
-    leds[i] += CRGB(
-      g * factor,
-      0,
-      b * factor
-    );
-  }
-}
+    double b = 32 * max(0, sin((i * 0.2) + (millis() * 0.00042)));
+    double r = 32 * max(0, sin((i * 0.2) + (millis() * 0.00036)));
+    double g = 32 * max(0, sin((i * 0.2) + (millis() * 0.00031)));
 
-void seagreen(float factor) {
-  for (int i=0; i<NUM_LEDS; i++) {
-    double b = 32 * (1 + sin((i * 0.2) + (millis() * 0.00042)));
-    double g = 32 * (1 + sin((i * 0.2) + (millis() * 0.00036)));
   //      Serial.println(g);
     leds[i] += CRGB(
-      0,
-      g * factor,
+      r * factor,
+      g,
       b * factor
     );
   }
